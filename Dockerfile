3cb2981cc802228c860df29312e44bc7813bfc42
Precious <odukwechiemeka@gmail.com>
2024-04-28 22:17:42 +0100

    final commit

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,15 @@
-# Build the app in image ‘builder’ (multi-stage builds)
-FROM node:latest as build
+FROM node:latest
 
-# Define working directory
 WORKDIR /app
 
-# Duplicate the package-lock.json and package.json prior to other files
 COPY package*.json ./
 
-RUN npm ci
+COPY . .
 
 RUN npm install -g @angular/cli
-# Duplicate all necessary files
-COPY . .
-# Set up project dependencies
+
 RUN npm install
 
-# Compile the Angular application
-RUN npm run build –quiz-app
-
-# Use nginx server to deliver the application
-FROM nginx:latest
-
-# Transfer the output of the build step
-COPY ./nginx.conf /etc/nginx/conf.d/default.conf
-
-COPY –from=build /app/dist/my-angular-app/ /usr/share/nginx/html/
+CMD ["ng", "serve", "--host", "0.0.0.0"]
 
 EXPOSE 80
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import Parse from 'parse';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'quiz-app';
+  question: number = 1;
+  questionAnswer: string = "";
+
+  data: any = [];
+
+  fetchData = async () => {
+    try {
+      const query = new Parse.Query("Quiz");
+      const quiz = await query.find();
+      this.data = quiz;
+      console.log(this.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  handleSubmit = () => {
+    console.log(this.data[this.question].get("answer"));
+    console.log(this.questionAnswer)
+    if(this.questionAnswer == this.data[this.question].get("answer")) {
+      console.log('fire');
+      // let newQuestion = this.question + 1;
+      // this.question = newQuestion;
+      // console.log(this.question);
+      // this.fetchData();
+    }
+  }
+
+  ngOnInit() {
+    console.log("lifecycle");
+    this.fetchData();
+  }
+
 }
+
+Parse.initialize("Nk6O6jwBL2mStRBnYnrSsqEfz28to8bBC7B7tNjB", "aeTiH8yTBPnJsU06rn96n1en6axhon0TTWGuzjAc");
+Parse.serverURL = "https://parseapi.back4app.com/";
